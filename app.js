c79d0a7ddd8c769e22eb007ff53322a574bef2c6
Samy ZIANE <samy.ziane-pro@outlook.com>
2025-06-26 17:01:24 +0200

    update

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('/api/health', (req, res) => {
   res.json({ 
     status: 'OK', 
     message: 'MSCG Backend',
-    models: ['User', 'Simulation']
+    models: ['User', 'Simulation', 'SimulationResults']
   });
 });
 
